a511fabaaf53be8607d9889c15a291ffebbb9978
arhama70 <arhama70@kprschools.ca>
2023-12-13 09:32:49 -0500

    All Done, add puase button and functions

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ def draw_text(text, font, text_col, x , y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    score = 0
+    return score
+
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -114,8 +121,21 @@ class Button():
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
     def draw(self):
+
+        action = False
+
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+
+        #check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
         #draw button
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
 
 
 bird_group = pygame.sprite.Group()
@@ -184,7 +204,11 @@ while run:
 
         pipe_group.update()
 
-
+    #check for game over and reset
+    if game_over == True:
+        if button.draw() == True:
+            game_over = False
+            score = reset_game()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
